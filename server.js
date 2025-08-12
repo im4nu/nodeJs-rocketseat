3ba86b03293a110e06c4c612b97b2043d718f771
manu <victorx811@gmail.com>
2025-08-12 18:28:17 -0300

    added find course by id path

--- server.js
+++ server.js
@@ -13,8 +13,23 @@ server.get("/courses", () => {
   return courses;
 });
 
-server.post("/courses", () => {
-  courses.push({ id: crypto.randomUUID, title: "Novo curso" });
+server.post("/courses", (request, reply) => {
+  const courseId = crypto.randomUUID();
+
+  courses.push({ id: courseId, title: "Novo curso" });
+
+  return reply.status(201).send({ courseId });
+});
+
+server.get("/courses/:id", (request, reply) => {
+  const params = request.params.id;
+  const findCourseById = courses.find((item) => item.id === params);
+
+  if (findCourseById) {
+    return { findCourseById };
+  }
+
+  return reply.status(404).send();
 });
 
 server.listen({ port: 3333 }).then(() => {
